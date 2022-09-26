448287a7b224224282e3ebc2cb9bb468b7ab89ba
soobeen27 <youdew27@icloud.com>
2022-09-26 17:30:55 +0900

    Initial Commit

---
+++ loginProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginProject
+//
+//  Created by Soo Jang on 2022/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
